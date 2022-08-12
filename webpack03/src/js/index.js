4cb461f345d53de6482f05ac947656c3d0d26efb
AngeloAugustoCSW <angelo.weimar@hotmail.com>
2022-08-11 21:53:16 -0300

    Webpack03-Class

---
+++ webpack03/src/js/index.js
@@ -0,0 +1,6 @@
+import "../scss/style.scss"
+console.log("If you somehow see this, buy PAYDAY2. NOW")
+const app = document.getElementById("app");
+const hello = document.createElement('h1');
+hello.innerHTML = "Buy Payday2, NOW"
+app.append(hello)
